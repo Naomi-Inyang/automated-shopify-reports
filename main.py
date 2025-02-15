274d1669f011b175ef605a580c10c45c8a342c2f
Eno-Inyang <ibukuninyang@gmail.com>
2025-02-15 15:29:40 +0100

    format store report message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests, os
 
 load_dotenv()
 
-SHOPIFY_STORE_DOMAIN = os.getenv('STORE_URL')
+SHOPIFY_STORE_URL = os.getenv('STORE_URL')
 API_KEY = os.getenv('API_KEY')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')  
 SECRET_KEY= os.getenv('SECRET_KEY') 
@@ -12,32 +12,43 @@ HEADER_VALUES = {
     'X-Shopify-Access-Token': ACCESS_TOKEN
 }
 
+def fetch_order_info():
+    url = f'{SHOPIFY_STORE_URL}/admin/api/2025-01/reports.json'
+    response = requests.get(url, headers=HEADER_VALUES)
 
-# def fetch_reports():
-#     url = f"{BASE_URL}/reports.json"
-#     response = requests.get(url)
-#     return response.json() if response.status_code == 200 else response.text
+    if response.status_code == 200:
+        orders = response.json().get('orders', [])
+        total_sales = sum(float(order['total_price']) for order in orders)
+        total_orders = len(orders)
+    else:
+        total_sales, total_orders = 0, 0
 
-# # Fetch Orders Data (Month-to-Date)
-# def fetch_orders():
-#     url = f"{BASE_URL}/orders.json?status=any&created_at_min=2024-02-01"
-#     response = requests.get(url)
-#     return response.json() if response.status_code == 200 else response.text
+    return total_sales, total_orders
 
-# # Fetch Shop Info
-# def fetch_shop_info():
-#     url = f"{BASE_URL}/shop.json"
-#     response = requests.get(url)
+def fetch_store_name():
+    url = f'{SHOPIFY_STORE_URL}/admin/api/2025-01/shop.json'
+    response = requests.get(url, headers=HEADER_VALUES)
 
-response = requests.get(f'{SHOPIFY_STORE_DOMAIN}/admin/api/2025-01/reports.json', headers=HEADER_VALUES)
+    if response.status_code == 200:
+        store_name = response.json().get('shop', {}).get('name', "Your Shopify Store")
+    else:
+        store_name = "Your Shopify Store"
 
-print(response.json())
+    return store_name
 
-# if __name__ == "__main__":
-#     reports_data = fetch_reports()
-#     orders_data = fetch_orders()
-#     shop_info = fetch_shop_info()
+def generate_store_report():
+    shop_name = fetch_store_name()
+    total_sales, total_orders = fetch_order_info()
 
-#     print("Shop Info:", shop_info)
-#     print("Reports Data:", reports_data)
-#     print("Orders Data:", orders_data)
+    store_report = f'''
+        🚀 *Shopify Report for {shop_name}* 🚀
+
+        💰 *Total Sales MTD:* ${total_sales}  
+        📦 *Total Orders MTD:* {total_orders}  
+    '''
+
+    return store_report
+
+if __name__ == "__main__":
+    reports_data = generate_store_report()
+    print("Reports Data:", reports_data)
